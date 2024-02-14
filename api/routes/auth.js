f04b8f7375b98ebb0efdcec53edac70d2dd8ed2e
James Derrick <32356714+jamesderrick@users.noreply.github.com>
2024-02-14 13:54:54 +0000

    revert localstorage and scope changes

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -11,7 +11,7 @@ const { REDIRECT_URI, POST_LOGOUT_REDIRECT_URI } = require('../authConfig');
 const router = express.Router();
 
 router.get('/signin', authProvider.login({
-    scopes: ['26c3f5c0-2a80-4ca1-b814-3e819799b996'],
+    scopes: [],
     redirectUri: REDIRECT_URI,
     successRedirect: '/'
 }));
